1191ae9273d919b93a9714006a911c18cc47471d
LAPTOP-1J3G52L7\Ren Yifan <renyifan008@gmail.com>
2018-11-14 14:26:16 +0800

    Update partical system

---
+++ IndieGameProjectUnity/Assets/scripts/AtackSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackSystem : MonoBehaviour {
+
+    public ParticleSystem P1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        skill();
+	}
+
+    public void skill() {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            P1.Play();
+        }
+    }
+}
--- IndieGameProjectUnity/Assets/scripts/PlayerController.cs
+++ IndieGameProjectUnity/Assets/scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
     public VirturalJoystick vj;
     Rigidbody2D rb2d;
     public float movementSpeed;
+    
 
     // Use this for initialization
     void Start () {
